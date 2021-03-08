35bbaf84312816bd839f2b5a901dcb841d88373d
BazovaLida <lydiaofficialv@gmail.com>
2021-03-08 11:42:35 +0200

    lab4 is done

---
+++ app/src/main/java/ua/kpi/comsys/iv8101/BookActivity.java
@@ -0,0 +1,89 @@
+package ua.kpi.comsys.iv8101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class BookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle("Book");
+
+        Book currBook = getBookFullInfo();
+
+        TextView tw = findViewById(R.id.book_info);
+        tw.setText(Html.fromHtml(currBook.getInfo()));
+
+        ImageView imageView = findViewById(R.id.book_image);
+        imageView.setImageResource(currBook.getImageID());
+    }
+
+    private Book getBookFullInfo(){
+
+        int fileID = getIntent().getExtras().getInt("file");
+        Scanner scanner = new Scanner(getResources().openRawResource(fileID));
+        try {
+            String data = scanner.nextLine();
+            JSONObject jsonObject = new JSONObject(data);
+
+            String title = jsonObject.getString("title");
+            String subtitle = jsonObject.getString("subtitle");
+            String authors = jsonObject.getString("authors");
+            String publisher = jsonObject.getString("publisher");
+            String pages = jsonObject.getString("pages");
+            String year = jsonObject.getString("year");
+            String rating = jsonObject.getString("rating");
+            String desc = jsonObject.getString("desc");
+            String price = jsonObject.getString("price");
+            String isbn13 = jsonObject.getString("isbn13");
+            String image = jsonObject.getString("image").toLowerCase();
+
+
+            int formatIndex = image.lastIndexOf(".");
+            if(formatIndex == -1)
+                formatIndex = 0;
+            String img = image.substring(0, formatIndex);
+
+            int imageID = getResources().getIdentifier(img, "drawable", getPackageName());
+            Book currBook = new Book(title, subtitle, isbn13, price, imageID);
+            currBook.setInfo(authors, publisher, pages, year, rating, desc);
+            return currBook;
+
+        } catch (JSONException e) {
+            Toast.makeText(this, "JSON exception!", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        } catch (NoSuchElementException e){
+            Toast.makeText(this, "Exception while scanning file!", Toast.LENGTH_SHORT).show();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+}
--- app/src/main/java/ua/kpi/comsys/iv8101/ListViewController.java
+++ app/src/main/java/ua/kpi/comsys/iv8101/ListViewController.java
@@ -1,90 +1,242 @@
 package ua.kpi.comsys.iv8101;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
-import java.util.Objects;
 import java.util.Scanner;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 
-public class ListViewController extends Fragment {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+public class ListViewController extends Fragment implements BooksAdapter.OnBookListener  {
+    private static final ArrayList<Book> library = new ArrayList<>();
+    private static BooksAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frag_third, container, false);
+        setHasOptionsMenu(true);
+        View rootView = inflater.inflate(R.layout.frag_third, container, false);
+
+        return rootView;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        Context context = Objects.requireNonNull(getActivity()).getApplicationContext();
+        Toolbar toolbar = (Toolbar)view.findViewById(R.id.toolbar);
+        toolbar.setTitle("Search");
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
 
         RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        setUpRecyclerView(recyclerView, view);
+    }
+
+    private void setUpRecyclerView(RecyclerView recyclerView, View view) {
+        Context context = requireActivity().getApplicationContext();
+        if(library.isEmpty())
+            createLibrary(context);
+        adapter = new BooksAdapter(library, this);
 
         RecyclerView.ItemDecoration itemDecoration = new
                 DividerItemDecoration(context, DividerItemDecoration.VERTICAL);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
         recyclerView.addItemDecoration(itemDecoration);
 
 
+        ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new
+                ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            Drawable background;
+            Drawable xMark;
+            int xMarkMargin;
+            boolean initiated;
+
+            private void init() {
+                background = new ColorDrawable(Color.RED);
+                xMark = ContextCompat.getDrawable(context, R.drawable.ic_delete);
+                xMark.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+                initiated = true;
+            }
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                Toast.makeText(getContext(), "Deleted book " + (position + 1), Toast.LENGTH_SHORT).show();
+                library.remove(position);
+                adapter.changeList(library);
+            }
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                View itemView = viewHolder.itemView;
+
+                // not sure why, but this method get's called for viewholder that are already swiped away
+                if (viewHolder.getAdapterPosition() == -1) {
+                    // not interested in those
+                    return;
+                }
+
+                if (!initiated) {
+                    init();
+                }
+
+                // draw red background
+                background.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+                background.draw(c);
+
+                // draw x mark
+                int itemHeight = itemView.getBottom() - itemView.getTop();
+                int intrinsicWidth = xMark.getIntrinsicWidth();
+                int intrinsicHeight = xMark.getIntrinsicWidth();
+
+                int xMarkLeft = itemView.getRight() - xMarkMargin - intrinsicWidth;
+                int xMarkRight = itemView.getRight() - xMarkMargin;
+                int xMarkTop = itemView.getTop() + (itemHeight - intrinsicHeight)/2;
+                int xMarkBottom = xMarkTop + intrinsicHeight;
+                xMark.setBounds(xMarkLeft, xMarkTop, xMarkRight, xMarkBottom);
+
+                xMark.draw(c);
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(recyclerView);
+
+    }
+    private void createLibrary(Context context) {
         Scanner scanner = new Scanner(getResources().openRawResource(R.raw.bookslist));
         try {
             String data = scanner.nextLine();
             JSONObject jsonObject = new JSONObject(data);
-            ArrayList<Book> library = processingJsonObj(jsonObject);
 
-            recyclerView.setAdapter(new BooksAdapter(library));
+            JSONArray booksInJSON = jsonObject.getJSONArray("books");
+            for (int i = 0; i < booksInJSON.length(); i++) {
+                JSONObject c = booksInJSON.getJSONObject(i);
+                String title = c.getString("title");
+                String subtitle = c.getString("subtitle");
+                String isbn13 = c.getString("isbn13");
+                String price = c.getString("price");
+                String image = c.getString("image").toLowerCase();
+
+
+                int formatIndex = image.lastIndexOf(".");
+                if(formatIndex == -1)
+                    formatIndex = 0;
+                String img = image.substring(0, formatIndex);
+
+                int imageID = getResources().getIdentifier(img, "drawable", getContext().getPackageName());
+                library.add(new Book(title, subtitle, isbn13, price, imageID));
+            }
         } catch (JSONException e) {
             Toast.makeText(context, "JSON exception!", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         } catch (NoSuchElementException e){
             Toast.makeText(context, "Exception while scanning file!", Toast.LENGTH_SHORT).show();
         }
-
+    }
+    public static void addBook(Book book){
+        library.add(book);
+        adapter.changeList(library);
     }
 
-    private ArrayList<Book> processingJsonObj(JSONObject jsonObject) throws JSONException {
-        JSONArray booksInJSON = jsonObject.getJSONArray("books");
-        ArrayList<Book> library = new ArrayList<>();
-        for (int i = 0; i < booksInJSON.length(); i++) {
-            JSONObject c = booksInJSON.getJSONObject(i);
-            String title = c.getString("title");
-            String subtitle = c.getString("subtitle");
-            String isbn13 = c.getString("isbn13");
-            String price = c.getString("price");
-            String image = c.getString("image").toLowerCase();
+    @Override
+    public void onBookClick(int position) {
+        String isbn = library.get(position).getIsbn13();
+        Resources res  = getContext().getResources();
+        int infoId = res.getIdentifier(isbn, "raw", getContext().getPackageName());
 
-
-            int formatIndex = image.lastIndexOf(".");
-            if(formatIndex == -1)
-                formatIndex = 0;
-            String img = image.substring(0, formatIndex);
-
-            int imageID = getResources().getIdentifier(img, "drawable", getContext().getPackageName());
-            library.add(new Book(title, subtitle, isbn13, price, imageID));
+        if(infoId != 0) {
+            Intent intent = new Intent(requireActivity(), BookActivity.class);
+            intent.putExtra("file", infoId);
+            startActivity(intent);
         }
-        Toast.makeText(getActivity().getApplicationContext(), "here", Toast.LENGTH_SHORT).show();
-        return library;
+    }
+
+    // calling on create option menu layout to inflate our menu file.
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        // inside inflater we are inflating our menu file.
+        menuInflater.inflate(R.menu.search_menu, menu);
+
+        // below line is to get our menu item.
+        MenuItem searchItem = menu.findItem(R.id.actionSearch);
+
+        // getting search view of our item.
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        // below line is to call set on query text listener method.
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                ArrayList<Book> filtered = new ArrayList<>();
+
+                // running a for loop to compare elements.
+                for (Book item : library) {
+                    // checking if the entered string matched with any item of our recycler view.
+                    if (item.getTitle().toLowerCase().contains(newText.toLowerCase())) {
+                        // if the item is matched, adding it to our filtered list.
+                        filtered.add(item);
+                    }
+                }
+                if (filtered.isEmpty()) {
+                    Toast.makeText(getContext(), "No Book Found.", Toast.LENGTH_SHORT).show();
+                }
+                adapter.changeList(filtered);
+                return false;
+            }
+        });
+
+        MenuItem addItem = menu.findItem(R.id.actionAddBook);
+        addItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = new Intent(requireActivity(), AddBookActivity.class);
+                startActivity(intent);
+                return true;
+            }
+        });
 
     }
+
 }
